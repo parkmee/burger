e475f82777d4539b178d035316e5d3bf5d859d1c
MMPark <parkmee@gmail.com>
2018-11-29 02:06:50 -0500

    updated schema with current timestamp

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,11 @@
 CREATE DATABASE burgers_db;
 
+USE burgers_db;
+
 CREATE TABLE burgers (
     id int auto_increment NOT NULL,
     burger_name varchar(20) NOT NULL,
     devoured boolean DEFAULT false,
-    createdAt TIMESTAMP NOT NULL,
+    createdAt TIMESTAMP NOT NULL DEFAULT current_timestamp,
     PRIMARY KEY(id)
 );
