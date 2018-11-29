97ef28f27e75eb10b21b15120f3ee539fee72d49
MMPark <parkmee@gmail.com>
2018-11-29 01:55:35 -0500

    updated schema for jawsdb

--- db/schema.sql
+++ db/schema.sql
@@ -1,9 +1,9 @@
 CREATE DATABASE burgers_db;
 
-USE burgers_db;
-
 CREATE TABLE burgers (
-    id int auto_increment primary key,
-    burger_name varchar(20) not null,
-    devoured boolean
+    id int auto_increment NOT NULL,
+    burger_name varchar(20) NOT NULL,
+    devoured boolean DEFAULT false,
+    createdAt TIMESTAMP NOT NULL,
+    PRIMARY KEY(id)
 );
